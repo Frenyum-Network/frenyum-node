5c3db5bf222045241632a3a2aee60f82536a5b07
mefreanzy <freanzyme@gmail.com>
2024-02-11 15:04:55 +0300

    Add: Unit test.

--- core/utils/src/timestamp.rs
+++ core/utils/src/timestamp.rs
@@ -1,4 +1,5 @@
 use std::time::{SystemTime, UNIX_EPOCH};
+use chrono::{DateTime, Utc};
 use std::fmt;
 use std::str::FromStr;
 
@@ -19,11 +20,16 @@ impl Timestamp
     {
         self.0 = 0;
     }
-    
-    pub fn elapsed(&self) -> u64
+
+    pub fn elapsed(&self) -> u64 
     {
         let now = Timestamp::now();
-        now.0 - self.0
+        if now >= *self
+        {
+            now.0 - self.0
+        } else {
+            self.0 - now.0
+        }
     }
 }
 
@@ -31,7 +37,10 @@ impl fmt::Display for Timestamp
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result
     {
-        write!(f, "{}", self.0)
+        let duration_since_epoch = std::time::Duration::from_secs(self.0);
+        let datetime = UNIX_EPOCH + duration_since_epoch;
+        let datetime: DateTime<Utc> = datetime.into();
+        write!(f, "{}", datetime.format("%Y-%m-%d %H:%M:%S"))
     }
 }
 
@@ -110,5 +119,68 @@ impl std::cmp::Ord for Timestamp
     }
 }
 
+#[cfg(test)]
+mod test
+{
+    use super::*;
 
+    #[test]
+    fn test_now_returns_current_time()
+    {
+        let now = Timestamp::now();
+        let system_now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
+        assert_eq!(now.0, system_now);
+    }
+
+    #[test]
+    fn test_reset_sets_timestamp_to_zero()
+    {
+        let mut timestamp = Timestamp::now();
+        timestamp.reset();
+        assert_eq!(timestamp.0, 0);
+    }
+
+    #[test]
+    fn test_elapsed_function()
+    {
+        let start = Timestamp::now();
+        std::thread::sleep(std::time::Duration::from_secs(2));
+        let end = Timestamp::now();
+        let elapsed = start.elapsed();
+        assert_eq!(elapsed, 2);
+    }
+
+    #[test]
+    fn test_display_formats_timestap_correctly()
+    {
+        let timestamp = Timestamp(123456789);
+        assert_eq!(format!("{}", timestamp), "1973-11-29 21:33:09");
+    }
+
+    #[test]
+    fn test_subtracting_timestamps_returns_correct_difference()
+    {
+        let timestamp1 = Timestamp(1000);
+        let timestamp2 = Timestamp(500);
+        let result = timestamp1 - timestamp2;
+        assert_eq!(result.0, 500);
+    }
+
+    #[test]
+    fn test_adding_duration_to_timestamp_returns_correct_result()
+    {
+        let timestamp = Timestamp(1000);
+        let result = timestamp + 500;
+        assert_eq!(result.0, 1500);
+    }
+
+    #[test]
+    fn test_timestamp_ord_trait_implementation()
+    {
+        let timestamp1 = Timestamp(1000);
+        let timestamp2 = Timestamp(2000);
+        assert!(timestamp1 < timestamp2);
+    }
+
+}
 
