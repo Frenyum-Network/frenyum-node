53f6fa6298d545cf2512abd4a45fcb96fbd60f1f
mefreanzy <freanzyme@gmail.com>
2024-02-11 03:35:08 +0300

    Feature: Block header structure added as a draft

---
+++ core/primvites/src/block_header.rs
@@ -0,0 +1,14 @@
+pub struct BlockHeader
+{
+    // Version
+    // Parent Hash
+    // Block Number
+    // Difficulty
+    // Block Hash
+    // Timestamp
+    // Nonce
+    // Total Difficulty
+    // Gas Price
+    // Gas Limit
+    // Transaction Root
+}
