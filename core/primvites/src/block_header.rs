332f2a3f0aea97fb8687f777e442c6a0a4782fa1
mefreanzy <freanzyme@gmail.com>
2024-02-15 20:48:40 +0300

    Update: Getter functions defined.

--- core/primvites/src/block_header.rs
+++ core/primvites/src/block_header.rs
@@ -4,16 +4,43 @@ use crate::{BlockNumber, BlockHeight, U256};
 
 pub struct BlockHeader
 {
+    hash: HashDigest,
     protocol_version: u32,
     parent_hash: HashDigest,
     block_number: BlockNumber,
     block_height: BlockHeight,
     difficulty: U256,
-    hash: HashDigest,
     timestamp: Timestamp,
     nonce: U256,
     total_difficulty: U256,
     gas_used: Gas,
     gas_limit: Gas,
-    // Transaction Root
+    transaction_root: HashDigest,
+}
+
+impl BlockHeader
+{
+    pub fn hash(&self) -> &HashDigest { &self.hash }
+
+    pub fn protocol_version(&self) -> u32 { self.protocol_version }
+
+    pub fn parent_hash(&self) -> &HashDigest { &self.parent_hash }
+
+    pub fn block_number(&self) -> BlockNumber { self.block_number }
+
+    pub fn block_height(&self) -> BlockHeight { self.block_height }
+
+    pub fn difficulty(&self) -> &U256 { &self.difficulty }
+
+    pub fn timestamp(&self) -> &Timestamp { &self.timestamp }
+
+    pub fn nonce(&self) -> &U256 { &self.nonce }
+
+    pub fn total_difficulty(&self) -> &U256 { &self.total_difficulty }
+
+    pub fn gas_used(&self) -> &Gas { &self.gas_used }
+
+    pub fn gas_limit(&self) -> &Gas { &self.gas_limit }
+
+    pub fn transaction_root(&self) -> &HashDigest { &self.transaction_root }
 }
