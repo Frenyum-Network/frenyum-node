343d5633f0e5f37927735dee13705b4d32057b81
mefreanzy <freanzyme@gmail.com>
2024-02-17 14:44:35 +0300

    Fix: Error fixed.

--- core/primvites/src/block_header.rs
+++ core/primvites/src/block_header.rs
@@ -47,35 +47,18 @@ impl BlockHeader
 
 pub struct BlockHeaderBuilder 
 {
-    hash: Option<HashDigest>,
-    protocol_version: Option<u32>,
-    parent_hash: Option<HashDigest>,
-    block_number: Option<BlockNumber>,
-    block_height: Option<BlockHeight>,
-    difficulty: Option<U256>,
-    timestamp: Option<Timestamp>,
-    nonce: Option<U256>,
-    total_difficulty: Option<U256>,
-    gas_used: Option<Gas>,
-    gas_limit: Option<Gas>,
-    transaction_root: Option<HashDigest>,
-}
-
-#[derive(Debug)]
-pub enum BuilderError
-{
-    HashMissing,
-    ProtocolVersionMissing,
-    ParentHashMissing,
-    BlockNumberMissing,
-    BlockHeightMissing,
-    DifficultyMissing,
-    TimestampMissing,
-    NonceMissing,
-    TotalDifficultyMissing,
-    GasUsedMissing,
-    GasLimitMissing,
-    TransactionRootMissing,
+    hash: HashDigest,
+    protocol_version: u32,
+    parent_hash: HashDigest,
+    block_number: BlockNumber,
+    block_height: BlockHeight,
+    difficulty: U256,
+    timestamp: Timestamp,
+    nonce: U256,
+    total_difficulty: U256,
+    gas_used: Gas,
+    gas_limit: Gas,
+    transaction_root: HashDigest,
 }
 
 impl BlockHeaderBuilder
@@ -83,138 +66,138 @@ impl BlockHeaderBuilder
     pub fn new() -> Self
     {
         Self {
-            hash: None,
-            protocol_version: None,
-            parent_hash: None,
-            block_number: None,
-            block_height: None,
-            difficulty: None,
-            timestamp: None,
-            nonce: None,
-            total_difficulty: None,
-            gas_used: None,
-            gas_limit: None,
-            transaction_root: None,
+            hash: Default::default(),
+            protocol_version: 0,
+            parent_hash: Default::default(),
+            block_number: 0,
+            block_height: 0,
+            difficulty: Default::default(),
+            timestamp: Default::default(),
+            nonce: Default::default(),
+            total_difficulty: Default::default(),
+            gas_used: Default::default(),
+            gas_limit: Default::default(),
+            transaction_root: Default::default(),
         }  
     }
 
     pub fn set_hash(&mut self, hash: HashDigest) -> &mut Self
     {
-        self.hash = Some(hash);
+        self.hash = hash;
         self
     }
 
     pub fn set_protocol_version(&mut self, version: u32) -> &mut Self
     {
-        self.protocol_version = Some(version);
+        self.protocol_version = version;
         self
     }
 
     pub fn set_parent_hash(&mut self, hash: HashDigest) -> &mut Self
     {
-        self.parent_hash = Some(hash);
+        self.parent_hash = hash;
         self
     }
 
     pub fn set_block_number(&mut self, number: BlockNumber) -> &mut Self
     {
-        self.block_number = Some(number);
+        self.block_number = number;
         self
     }
 
     pub fn set_block_height(&mut self, height: BlockHeight) -> &mut Self
     {
-        self.block_height = Some(height);
+        self.block_height = height;
         self
     }
 
     pub fn set_difficulty(&mut self, difficulty: U256) -> &mut Self
     {
-        self.difficulty = Some(difficulty);
+        self.difficulty = difficulty;
         self
     }
 
     pub fn set_timestamp(&mut self, timestamp: Timestamp) -> &mut Self
     {
-        self.timestamp = Some(timestamp);
+        self.timestamp = timestamp;
         self
     }
 
     pub fn set_nonce(&mut self, nonce: U256) -> &mut Self
     {
-        self.nonce = Some(nonce);
+        self.nonce = nonce;
         self
     }
 
     pub fn set_total_difficulty(&mut self, total_difficulty: U256) -> &mut Self
     {
-        self.total_difficulty = Some(total_difficulty);
+        self.total_difficulty = total_difficulty;
         self
     }
 
     pub fn set_gas_used(&mut self, gas_used: Gas) -> &mut Self
     {
-        self.gas_used = Some(gas_used);
+        self.gas_used = gas_used;
         self
     }
 
     pub fn set_gas_limit(&mut self, gas_limit: Gas) -> &mut Self
     {
-        self.gas_limit = Some(gas_limit);
+        self.gas_limit = gas_limit;
         self
     }
 
     pub fn set_transaction_root(&mut self, transaction_root: HashDigest) -> &mut Self  
     {
-        self.transaction_root = Some(transaction_root);
+        self.transaction_root = transaction_root;
         self
     }
 
-    pub fn build(&self) -> Result<BlockHeader, BuilderError>
+    pub fn build(&self) -> BlockHeader
     {
-        let hash = self.hash.ok_or(BuilderError::HashMissing)?;
-        let protocol_version = self.protocol_version.ok_or(BuilderError::ProtocolVersionMissing)?;
-        let parent_hash = self.parent_hash.ok_or(BuilderError::ParentHashMissing)?;
-        let block_number = self.block_number.ok_or(BuilderError::BlockNumberMissing)?;
-        let block_height = self.block_height.ok_or(BuilderError::BlockHeightMissing)?;
-        let difficulty = self.difficulty.ok_or(BuilderError::DifficultyMissing)?;
-        let timestamp = self.timestamp.ok_or(BuilderError::TimestampMissing)?;
-        let nonce = self.nonce.ok_or(BuilderError::NonceMissing)?;
-        let total_difficulty = self.total_difficulty.ok_or(BuilderError::TotalDifficultyMissing)?;
-        let gas_used = self.gas_used.ok_or(BuilderError::GasUsedMissing)?;
-        let gas_limit = self.gas_limit.ok_or(BuilderError::GasLimitMissing)?;
-        let transaction_root = self.transaction_root.ok_or(BuilderError::TransactionRootMissing)?;
+        let hash = self.hash;
+        let protocol_version = self.protocol_version;
+        let parent_hash = self.parent_hash;
+        let block_number = self.block_number;
+        let block_height = self.block_height;
+        let difficulty = self.difficulty;
+        let timestamp = self.timestamp;
+        let nonce = self.nonce;
+        let total_difficulty = self.total_difficulty;
+        let gas_used = self.gas_used;
+        let gas_limit = self.gas_limit;
+        let transaction_root = self.transaction_root;
 
-        Ok(BlockHeader {
-            hash: self.hash.clone().unwrap(),
-            protocol_version: self.protocol_version.unwrap(),
-            parent_hash: self.parent_hash.clone().unwrap(),
-            block_number: self.block_number.unwrap(),
-            block_height: self.block_height.unwrap(),
-            difficulty: self.difficulty.clone().unwrap(),
-            timestamp: self.timestamp.clone().unwrap(),
-            nonce: self.nonce.clone().unwrap(),
-            total_difficulty: self.total_difficulty.clone().unwrap(),
-            gas_used: self.gas_used.clone().unwrap(),
-            gas_limit: self.gas_limit.clone().unwrap(),
-            transaction_root: self.transaction_root.clone().unwrap(),
-        })
+        BlockHeader {
+            hash,
+            protocol_version,
+            parent_hash,
+            block_number,
+            block_height,
+            difficulty,
+            timestamp,
+            nonce,
+            total_difficulty,
+            gas_used,
+            gas_limit,
+            transaction_root,
+        }
     }
     
     pub fn clear(&mut self)
     {
-        self.hash = None;
-        self.protocol_version = None;
-        self.parent_hash = None;
-        self.block_number = None;
-        self.block_height = None;
-        self.difficulty = None;
-        self.timestamp = None;
-        self.nonce = None;
-        self.total_difficulty = None;
-        self.gas_used = None;
-        self.gas_limit = None;
-        self.transaction_root = None;
+        self.hash = Default::default();
+        self.protocol_version = 0;
+        self.parent_hash = Default::default();
+        self.block_number = 0;
+        self.block_height = 0;
+        self.difficulty = Default::default();
+        self.timestamp = Default::default();
+        self.nonce = Default::default();
+        self.total_difficulty = Default::default();
+        self.gas_used = Default::default();
+        self.gas_limit = Default::default();
+        self.transaction_root = Default::default();
     }
     
 }
