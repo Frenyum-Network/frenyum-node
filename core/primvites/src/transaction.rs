9077de02f643838997a8bf26c0b804954bebf4e0
mefreanzy <freanzyme@gmail.com>
2024-02-25 04:16:22 +0300

    Add: Transaction draft created.

---
+++ core/primvites/src/transaction.rs
@@ -0,0 +1,19 @@
+pub struct RawTransaction
+{
+    // Chain ID
+    // Nonce
+    // Action
+    // Gas Price
+    // Gas
+    // Value
+    // Data
+}
+
+pub struct SignedTransaction
+{
+    // Timestamp
+    // Raw Transaction
+    // Signature
+    // Hash
+    // Size
+}
