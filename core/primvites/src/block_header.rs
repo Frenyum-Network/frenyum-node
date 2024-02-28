24df97063fc215a642bce091eef072570a4d1539
mefreanzy <freanzyme@gmail.com>
2024-02-28 23:06:30 +0300

    Add: Document.

--- core/primvites/src/block_header.rs
+++ core/primvites/src/block_header.rs
@@ -2,46 +2,71 @@ use crypto::hash::HashDigest;
 use utils::{gas::Gas, timestamp::Timestamp};
 use crate::{BlockNumber, BlockHeight, U256};
 
+// An Block header
 pub struct BlockHeader
-{
+{  
+    // Hash of the block
     hash: HashDigest,
+    // Protocol version used
     protocol_version: u32,
+    // Parent hash
     parent_hash: HashDigest,
+    // Block number
     block_number: BlockNumber,
+    // Block height
     block_height: BlockHeight,
+    // Difficulty level
     difficulty: U256,
+    // Block timestamp
     timestamp: Timestamp,
+    // Nonce of the block
     nonce: U256,
+    // Total block difficulty
     total_difficulty: U256,
+    // Used gas price
     gas_used: Gas,
+    // Gas limit
     gas_limit: Gas,
+    // Transaction root
     transaction_root: HashDigest,
 }
 
 impl BlockHeader
 {
+    // The 'hash' function returns the hash value of the block
     pub fn hash(&self) -> &HashDigest { &self.hash }
-
+    
+    // The `protocol_version` function returns the protocol version of the block
     pub fn protocol_version(&self) -> u32 { self.protocol_version }
 
+    // The `parent_hash` function returns the parent hash of the block
     pub fn parent_hash(&self) -> &HashDigest { &self.parent_hash }
 
+    // The `block_number` function returns the block number
     pub fn block_number(&self) -> BlockNumber { self.block_number }
 
+    // The `block_height` function returns the block height
     pub fn block_height(&self) -> BlockHeight { self.block_height }
 
+    // The `difficulty` function returns the difficulty of the block
     pub fn difficulty(&self) -> &U256 { &self.difficulty }
 
+    // The `timestamp` function returns the timestamp of the block
     pub fn timestamp(&self) -> &Timestamp { &self.timestamp }
 
+    // The `nonce` function returns the nonce of the block
     pub fn nonce(&self) -> &U256 { &self.nonce }
 
+    // The `total_difficulty` function returns the total difficulty of the block
     pub fn total_difficulty(&self) -> &U256 { &self.total_difficulty }
 
+    // The `gas_used` function returns the gas used in the block
     pub fn gas_used(&self) -> &Gas { &self.gas_used }
 
+    // The `gas_limit` function returns the gas limit of the block
     pub fn gas_limit(&self) -> &Gas { &self.gas_limit }
 
+    // The `transaction_root` function returns the transaction root of the block
     pub fn transaction_root(&self) -> &HashDigest { &self.transaction_root }
 }
 
@@ -86,6 +111,7 @@ impl Default for BlockHeader
 
 impl BlockHeaderBuilder
 {
+    // The 'new' function creates a new block header constructor
     pub fn new() -> Self
     {
         Self {
@@ -104,78 +130,91 @@ impl BlockHeaderBuilder
         }  
     }
 
+    // The 'set_hash' function sets the hash value of the block
     pub fn set_hash(&mut self, hash: HashDigest) -> &mut Self
     {
         self.hash = hash;
         self
     }
-
+    
+    // The `set_protocol_version` function sets the protocol version of the block
     pub fn set_protocol_version(&mut self, version: u32) -> &mut Self
     {
         self.protocol_version = version;
         self
     }
 
+    // The `set_parent_hash` function sets the parent hash value of the block
     pub fn set_parent_hash(&mut self, hash: HashDigest) -> &mut Self
     {
         self.parent_hash = hash;
         self
     }
 
+    // The `set_block_number` function sets the block number
     pub fn set_block_number(&mut self, number: BlockNumber) -> &mut Self
     {
         self.block_number = number;
         self
     }
 
+    // The `set_block_height` function sets the block height
     pub fn set_block_height(&mut self, height: BlockHeight) -> &mut Self
     {
         self.block_height = height;
         self
     }
 
+    // The `set_difficulty` function sets the difficulty of the block
     pub fn set_difficulty(&mut self, difficulty: U256) -> &mut Self
     {
         self.difficulty = difficulty;
         self
     }
 
+    // The `set_timestamp` function sets the timestamp of the block
     pub fn set_timestamp(&mut self, timestamp: Timestamp) -> &mut Self
     {
         self.timestamp = timestamp;
         self
     }
 
+    // The `set_nonce` function sets the nonce value of the block
     pub fn set_nonce(&mut self, nonce: U256) -> &mut Self
     {
         self.nonce = nonce;
         self
     }
 
+    // The `set_total_difficulty` function sets the total difficulty of the block
     pub fn set_total_difficulty(&mut self, total_difficulty: U256) -> &mut Self
     {
         self.total_difficulty = total_difficulty;
         self
     }
 
+    // The `set_gas_used` function sets the gas used in the block
     pub fn set_gas_used(&mut self, gas_used: Gas) -> &mut Self
     {
         self.gas_used = gas_used;
         self
     }
 
+    // The `set_gas_limit` function sets the gas limit of the block
     pub fn set_gas_limit(&mut self, gas_limit: Gas) -> &mut Self
     {
         self.gas_limit = gas_limit;
         self
     }
 
+    // The `set_transaction_root` function sets the transaction root of the block
     pub fn set_transaction_root(&mut self, transaction_root: HashDigest) -> &mut Self  
     {
         self.transaction_root = transaction_root;
         self
     }
-
+    
+    // The `build` function constructs a `BlockHeader` using the provided builder parameters
     pub fn build(&self) -> BlockHeader
     {
         let hash = self.hash;
