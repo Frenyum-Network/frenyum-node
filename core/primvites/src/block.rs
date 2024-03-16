e3d7a78ba3dd87671b9136ad3e0499436441fee7
mefreanzy <freanzyme@gmail.com>
2024-03-16 22:50:15 +0300

    add: display has been added to the block structure.

--- core/primvites/src/block.rs
+++ core/primvites/src/block.rs
@@ -1,6 +1,6 @@
 use crate::{block_header::BlockHeader, transaction::SignedTransaction};
 use utils::gas::Gas;
-use std::sync::Arc;
+use std::{sync::Arc, fmt::Formatter, fmt};
 
 pub struct Block
 {
@@ -25,6 +25,45 @@ impl Block
     pub fn body(&self) -> &BlockBody
     {
         &self.body
+    }
+}
+
+impl fmt::Display for Block
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result
+    {
+        write!(
+            f,
+            "Block Information:
+             Header: 
+               Hash: {}
+               Protocol Version: {}
+               Parent Hash: {}
+               Block Number: {}
+               Block Height: {}
+               Difficulty: {}
+               Timestamp: {}
+               Nonce: {}
+               Total Difficulty: {}
+               Transaction Root: {}
+             Body: 
+               Transactions: {:?}
+               Gas Used: {}
+               Gas Limit: {}",
+            self.header.hash(),
+            self.header.protocol_version(),
+            self.header.parent_hash(),
+            self.header.block_number(),
+            self.header.block_height(),
+            self.header.difficulty(),
+            self.header.timestamp(),
+            self.header.nonce(),
+            self.header.total_difficulty(),
+            self.header.transaction_root(),
+            self.body.transaction(),
+            self.body.gas_used(),
+            self.body.gas_limit()
+        )
     }
 }
 
@@ -62,5 +101,19 @@ impl BlockBody
     pub fn gas_limit(&self) -> &Gas
     {
         &self.gas_limit
+    }
+}
+
+impl fmt::Display for BlockBody 
+{
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result
+    {
+        write!(
+            formatter,
+            "Block Body: \nTransactions: {:?}\nGas Used: {:?}\nGas Limit: {:?}",
+            self.transaction,
+            self.gas_used,
+            self.gas_limit
+        )
     }
 }
