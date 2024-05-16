934df8d685876221816ce69294d64daf2eedff16
mefreanzy <freanzyme@gmail.com>
2024-05-16 06:45:38 +0300

    update: functions have been improved.

--- core/storage/src/rocksdb.rs
+++ core/storage/src/rocksdb.rs
@@ -1,3 +1,4 @@
+use utils::db::StoreConfig;
 use rocksdb::{DB, Options, WriteBatch, Direction, IteratorMode, ColumnFamily, ErrorKind, ColumnFamilyDescriptor};
 use std::path::Path;
 use std::collections::HashMap;
@@ -15,7 +16,7 @@ impl RocksDB
 {
     pub fn open(
         path: &Path, 
-        db_cf: HashMap<Column, ColumnFamily>
+        db_cf: H
     ) -> Result<Self, rocksdb::Error> {
         let db_opt = Options::default();
         let db = DB::open_default(path)?;
@@ -33,11 +34,11 @@ impl RocksDB
 
     fn open_with_columns(
         path: &Path,
-        db_opt: Options,
+        config: &StoreConfig
         db_cf: HashMap<Column, ColumnFamily>,
         columns: &[Column],
     ) -> Result<Self, rocksdb::Error> {
-        let (db, db_opt) = Self::open_db(path, db_opt, columns)?;
+        let (db, db_opt) = Self::open_db(path, config, columns)?;
         Ok(Self { db, db_opt, db_cf })
     }
 
@@ -53,49 +54,6 @@ impl RocksDB
         }).collect();   
         let db = DB::open_cf_descriptors(&db_opt, path, cf_descriptors)?;
         Ok((db, db_opt))
-    }
-
-    pub fn flush(&self) -> Result<(), rocksdb::Error>
-    {
-        Ok(self.db.flush()?)
-    }
-
-    pub fn compact(&self) -> Result<(), rocksdb::Error>
-    {
-        Ok(self.db.compact_range(None::<&[u8]>, None::<&[u8]>))
-    }
-
-    
-}
-
-// Key operations
-impl RocksDB
-{
-    pub fn get(
-        &self, 
-        column: Column, 
-        key: &[u8]
-    ) -> Result<Option<Vec<u8>>, rocksdb::Error> {
-        let cf = self.db_cf.get(&column).expect("Column family not found!");
-        self.db.get_cf(cf, key)
-    }
-
-    pub fn put(
-        &self,
-        column: Column,
-        key: &[u8],
-        value: &[u8]
-     ) -> Result<(), rocksdb::Error> {
-        let cf = self.db_cf.get(&column).expect("Column family not found!");
-        self.db.put_cf(cf, key, value)
-    }
-
-    pub fn delete(
-        &mut self,
-        column: Column,
-        key: &[u8]
-     ) -> Result<(), rocksdb::Error> {
-        let cf = self.db_cf.get(&column).expect("Column family not found!");
-        self.db.delete_cf(cf, key)
     } 
 }
+
