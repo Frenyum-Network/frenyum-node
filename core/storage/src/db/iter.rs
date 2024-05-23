0c7606e4a07cdc5a0089e42a7c388c49ab447316
mefreanzy <freanzyme@gmail.com>
2024-05-23 07:09:18 +0300

    refactor: supported by lines of commentary for clarity.

--- core/storage/src/db/iter.rs
+++ core/storage/src/db/iter.rs
@@ -1,20 +1,29 @@
+use crate::rocksdb::RocksDB;
+pub use rocksdb::{DBIterator, IteratorMode};
+
+// Structure for RocksDB Iteration
 pub struct RocksDBIterator<'a> 
 { 
-    iter: rocksdb::DBIterator<'a>,
+    iter: rocksdb::DBIterator<'a>, // Iterator for RocksDB
 }
 
+
 impl<'a> RocksDBIterator<'a>
-{
+{   
+    // Constructor function used to create a new RocksDBIterator instance
     pub fn new(iter: rocksdb::DBIterator<'a>) -> Self
     {
         RocksDBIterator { iter }
     }
 }
 
+// Implement the Iterator trait for RocksDBIterator
 impl<'a> Iterator for RocksDBIterator<'a>
 {
+    // The Item relation of the Iterator is a tuple of type Box<[u8]> for both key and value
     type Item = (Box<[u8]>, Box<[u8]>);
-
+    
+    // Function to get the next item
     fn next(&mut self) -> Option<Self::Item>
     {
         match self.iter.next() {
