fd7f560e75e49e5900b2abf8708cee1d3b88333e
mefreanzy <freanzyme@gmail.com>
2024-06-22 04:43:24 +0300

    refactor: get functions have been added.

--- core/storage/src/db/snapshot.rs
+++ core/storage/src/db/snapshot.rs
@@ -13,7 +13,23 @@ impl<'a> Snapshot<'a>
         let snapshot = unsafe { ffi::rocksdb_crate_snapshot(db.inner) };
         Snapshot { db, snapshot }
     }
-}
+
+    pub fn get<K: AsRef<[u8]>>(&self, key: K) -> Result<Option<<Vec<u8>>, Error>
+    {
+        let mut readopts = ReadOptions::default();
+        unsafe { ffi::rocksdb_readopitons_set_snapshot(readopts.inner(), self.snapshot) };
+        self.db.get_opt(key, &readopts)
+    }
+
+    pub fn get_cf<K: AsRef<[u8]>>(
+        &self,
+        cf: &ColumnFamily,
+        key: K,
+    ) -> Result<Option<Vec<u8>>, Error> {
+        let mut readopts = ReadOptions::default();
+        unsafe { ffi::rocksdb_readoptions_set_snapshot(readopts.inner(), self.snapshot) };
+        self.db.get_opt(key, &readopts)
+    }
 
 impl<'a> Drop for Snapshot<'a>
 {
