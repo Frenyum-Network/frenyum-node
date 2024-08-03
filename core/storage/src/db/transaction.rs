909a542ecfcaec70b2d2794262bef34da09b683c
mefreanzy <freanzyme@gmail.com>
2024-08-03 05:47:00 +0300

    update: add impl and functions.

--- core/storage/src/db/transaction.rs
+++ core/storage/src/db/transaction.rs
@@ -1,1 +1,13 @@
 pub struct RocksDBTransaction { pub(crate) inner: OptimisticTransactionDB}
+
+impl RocksDBTransaction
+{
+    pub fn get_pinned<K: AsRef<[u8]>(
+        &self,
+        col: &str,
+        key: K,
+    ) -> Result<Option<DBPinnableSlice>, Error> {
+        let cf_handle = self.inner.get_cf_handle(column)?;
+        self.inner.get_pinned_cf(cf_handle, key, &ReadOptions::default())
+    }
+}
