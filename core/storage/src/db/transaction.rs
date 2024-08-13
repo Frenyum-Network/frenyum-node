6a4fe39272b0c618133860bad9c0bf5bf99163ff
freanzy <freanzyme@gmail.com>
2024-08-13 03:43:48 +0300

    refactor: basic functions have been added.

--- core/storage/src/db/transaction.rs
+++ core/storage/src/db/transaction.rs
@@ -1,3 +1,5 @@
+use rocksdb::TransactionDB;
+
 pub struct RocksDBTransaction { pub(crate) inner: TransactionDB }
 
 impl RocksDBTransaction
@@ -10,6 +12,35 @@ impl RocksDBTransaction
         let cf_handle = self.inner.get_cf_handle(column)?;
         self.inner.get_pinned_cf(cf_handle, key, &ReadOptions::default())
     }
-
     
+    pub fn put()<K: AsRef<[u8]>, V: AsRef<[u8]>>(
+        &self,
+        col: &str,
+        key: K,
+        value: V,
+    ) -> Result<(), Error> {
+        let cf_handle = self.inner.get_cf_handle(col)?;
+        self.inner.put_cf(cf_handle, key, value)
+    }
+
+    pub fn delete()<K: AsRef<[u8]>>(
+        &self,
+        col: &str,
+        key: K,
+    ) -> Result<(), Error> {
+        let cf_handle = self.inner.get_cf_handle(col)?;
+        self.inner.delete_cf(cf_handle, key)
+    }
+
+    pub fn commit(&self) -> Result<(), Error> 
+    {
+        self.inner.commit()?;
+        Ok(())
+    }
+
+    pub fn rollback(&self) -> Result<(), Error>
+    {
+        self.inner.rollback()?;
+        Ok(())
+    }
 }
