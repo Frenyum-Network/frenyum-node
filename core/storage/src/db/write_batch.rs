c8dd555ca2723e4c8d65bcd77a7c13be6e411220
freanzy <freanzyme@gmail.com>
2024-08-21 05:00:56 +0300

    add: basic functions were implemented.

--- core/storage/src/db/write_batch.rs
+++ core/storage/src/db/write_batch.rs
@@ -1,5 +1,27 @@
+use rocksdb::{TransactionDB, WriteBatch}
+use std::sync::Arc;
+
 pub struct RocksDBWriteBatch
 {
     pub(crate) db: Arc<TransactionDB>,
     pub(crate) inner: WriteBatch,
 }
+
+impl RocksDBWriteBatch
+{
+    pub fn len(&self) -> usize
+    {
+        self.inner.len()
+    }
+
+    pub fn size_in_bytes(&self) -> usize
+    {
+        self.inner.size_in_bytes()
+    }
+
+    pub fn is_empty(&self) -> bool
+    {
+        self.inner.is_empty()
+    }
+}
+
