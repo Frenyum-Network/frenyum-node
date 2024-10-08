4683048702e315a3552b447d171fbb373a93457d
freanzy <freanzyme@gmail.com>
2024-10-08 06:13:00 +0300

    refactor: database administrator structure was created.

--- core/storage/src/db.rs
+++ core/storage/src/db.rs
@@ -4,3 +4,12 @@ pub mod snapshot;
 pub mod transaction;
 pub mod write_batch;
 
+use rocksdb::{DB, Options, WriteBatch, Snapshot, ColumnFamily, TransactionDB, Transaction, TransactionOptions, WriteOptions};
+use std::path::Path;
+
+pub struct DBManager
+{
+    db: TransactionDB,
+    write_batch: WriteBatch,
+}
+
