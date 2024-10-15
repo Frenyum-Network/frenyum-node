2faa99c710fc5e3e3a81f21769a1d31a8a508ce2
freanzy <freanzyme@gmail.com>
2024-10-15 06:52:30 +0300

    update: add comments to Transaction structure for better clarity.

--- core/storage/src/db/transaction.rs
+++ core/storage/src/db/transaction.rs
@@ -1,7 +1,10 @@
+// RocksDB transaction wrapper
 use rocksdb::{TransactionDB, DBPinnableSlice, Error, ReadOptions};
 
 pub struct RocksDBTransaction { pub(crate) inner: TransactionDB }
 
+// 'RocksDBTransaction' structure, transaction-based with TransactionDB
+// provides data management. Can perform atomic operations in the database.
 impl RocksDBTransaction
 {
     pub fn get_pinned<K: AsRef<[u8]>>(
@@ -13,6 +16,7 @@ impl RocksDBTransaction
         self.inner.get_pinned_cf(cf_handle, key, &ReadOptions::default())
     }
     
+    // The 'put' method writes data based on a specific column family and key.
     pub fn put<K: AsRef<[u8]>, V: AsRef<[u8]>>(
         &self,
         col: &str,
@@ -23,6 +27,7 @@ impl RocksDBTransaction
         self.inner.put_cf(cf_handle, key, value)
     }
 
+    // The 'delete' method deletes data based on a specific column family and key.
     pub fn delete<K: AsRef<[u8]>>(
         &self,
         col: &str,
@@ -32,12 +37,16 @@ impl RocksDBTransaction
         self.inner.delete_cf(cf_handle, key)
     }
 
+    // The 'commit' method will commit all changes made in the transaction.
+    // makes it permanent. If not committed, the changes are rolled back.
     pub fn commit(&self) -> Result<(), Error> 
     {
         self.inner.commit()?;
         Ok(())
     }
 
+    // The 'rollback' method rolls back transactions that have been committed.
+    // Uncommitted transactions are canceled.
     pub fn rollback(&self) -> Result<(), Error>
     {
         self.inner.rollback()?;
