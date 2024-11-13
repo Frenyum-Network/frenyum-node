9e47701ddb3037ef1d03383b8b0c1c53dbc16091
freanzy <freanzyme@gmail.com>
2024-11-14 00:29:18 +0300

    add: updated structure and added function to create structure.

--- core/storage/src/db.rs
+++ core/storage/src/db.rs
@@ -4,11 +4,25 @@ pub mod snapshot;
 pub mod transaction;
 pub mod write_batch;
 
-use rocksdb::{DB, Options, WriteBatch, Snapshot, ColumnFamily, TransactionDB, Transaction, TransactionOptions, WriteOptions};
-use std::path::Path;
+use utils::configs::db::StoreConfig;
+use rocksdb::db::{DB, Options, WriteBatch, ColumnFamily, TransactionDB, Error};
+use std::path::PathBuf;
 
 pub struct DBManager
 {
-    db: TransactionDB,
+    db: RocksDB,
 }
 
+impl DBManager 
+{
+    pub fn new(config: StoreConfig) -> Result<Self, rocksdb::error>
+    {
+        let options = config.to_options();
+        let path = config.path.unwrap_or_else(|| PathBuf::from("default_db_path"));
+        
+        let db = DB::open(&options, path);
+        Ok(Self {
+            db,
+        })
+    }
+}
